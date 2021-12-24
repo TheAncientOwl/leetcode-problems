edb1141f9dea52d600dad4a9679cf4523db0e55f
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-12-24 16:10:03 +0200

    solved 100.same-tree.hpp

--- Source.cpp
+++ Source.cpp
@@ -1,27 +1,23 @@
 #include "TreeNode.hpp"
 
-#include <vector>
-
 class Solution {
-private:
-  void inorder(TreeNode* root, std::vector<int>& sln) {
-    if (root != nullptr) {
-      inorder(root->left, sln);
-      sln.push_back(root->val);
-      inorder(root->right, sln);
-    }
-  }
-
 public:
-  std::vector<int> inorderTraversal(TreeNode* root) {
-    std::vector<int> sln;
-    this->inorder(root, sln);
-    return sln;
+  bool isSameTree(TreeNode* p, TreeNode* q) {
+    if (!p && !q)
+      return true;
+
+    if (!p || !q)
+      return false;
+
+    if (p->val != q->val)
+      return false;
+
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
   }
 };
 
-
 int main() {
+
 
   return 0;
 }
